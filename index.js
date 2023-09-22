7956122d3dd1e639e865bd9cf4a62402c5a1ef4c
faranbutt <faranbutt789@gmail.com>
2023-09-22 21:49:19 +0500

    Pushing dummy to repo

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+import express from 'express';
+
+const app = express();
+
 console.log("Faran")
 console.log(process.env.NAME);
+
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
